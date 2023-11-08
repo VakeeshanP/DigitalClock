b3432b2d6723667bb35cb85bfdfeb34a687ef787
Vakeeshan Paskaran <99076427+VakeeshanP@users.noreply.github.com>
2023-11-09 00:11:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ function displayTime() {
 
     let dateTime = new Date();
     let hr = dateTime.getHours();
-    let min = dateTime.getMinutes();
-    let sec = dateTime.getSeconds();
+    let min = padZero(dateTime.getMinutes());
+    let sec = padZero(dateTime.getSeconds());
 
     if (hr>12) {
         hr = hr-12
         ampm.innerHTML = 'PM'
     }
 
-    document.getElementById('Hours').innerHTML = hr
+    document.getElementById('Hours').innerHTML = padZero(hr)
     document.getElementById('Minutes').innerHTML = min
     document.getElementById('Seconds').innerHTML = sec
+
+}
+
+function padZero(num) {
+
+    return num < 10 ? "0" + num : num
 
 }
 
